3a31a55208b29607ee48007543b2c1d25d1be721
pavan <pavan.tanuku@sasi.ac.in>
2025-11-11 21:53:48 +0530

    day-1 basics

---
+++ day1/js1_1.js
@@ -0,0 +1,30 @@
+// 1st question
+let myName = "Pavan Mani Sankar";
+let age = 21;
+console.log('Hi, my name is', myName,'and I am', age, 'years old!');
+
+
+// 2nd question
+const college = 'Sasi Institute of Technology And Engineering';
+console.log('My college name is:', college);
+
+// 3rd question
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(`The sum of ${a} and ${b} is: ${c}`);
+
+// 4th question
+let isLoggedIn = true;
+if(isLoggedIn) {
+    console.log("The user is logged In!");
+} else {
+    console.log('The user is logged Out!');
+}
+
+
+// 5th question
+let container;
+console.log(`The container has "${container}"`);
+
+
---
+++ day1/js1_2.js
@@ -0,0 +1,27 @@
+// 6th question
+const student = {
+    name: "Alice",
+    age: 100,
+    branch: 'Earth'
+}
+console.log(student);
+
+// 7th question
+const favFoods = ['tiffin', 'lunch', 'dinner'];
+console.log(`The 3rd one of favFoods is "${favFoods[2]}"`);
+
+// 8th question
+
+
+console.log(typeof numberInt);
+console.log(typeof numberFloat);
+console.log(typeof string);
+console.log(typeof char);
+console.log(typeof isTrue);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log(typeof container);
+console.log(typeof hasValue);
+console.log(typeof greet);
+console.log(typeof notANumber);
+console.log(typeof symbol);
